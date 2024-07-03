5faf165f8508aa153e1d3f98c6947d4b3e21d679
kinamura <i.kosuke0801@gmail.com>
2024-07-03 11:47:54 +0900

    check lib

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/04 16:50:06 by kinamura          #+#    #+#             */
-/*   Updated: 2024/06/29 07:10:51 by kinamura         ###   ########.fr       */
+/*   Created: 2024/07/03 11:26:46 by kinamura          #+#    #+#             */
+/*   Updated: 2024/07/03 11:35:51 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include "libft.h"
+# include "libft.h"
+# include <limits.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 256
@@ -24,8 +24,8 @@
 
 char	*get_next_line(int fd);
 char	*ft_read_file(int fd, char *str);
-char	*ft_stradd(char *s1, char *s2);
-char	*ft_get_line(char *str);
 char	*ft_next_str(char *str);
+char	*ft_get_line(char *str);
+char	*ft_memadd(char *s1, const char *s2, size_t size);
 
 #endif
