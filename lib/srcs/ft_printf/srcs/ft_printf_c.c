ba752e7051c8a968ad15dc0d148cb89c08211f65
kinamura <i.kosuke0801@gmail.com>
2024-08-27 00:31:11 +0900

    create tmp

---
+++ lib/srcs/ft_printf/srcs/ft_printf_c.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_c.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 03:04:34 by kinamura          #+#    #+#             */
+/*   Updated: 2024/08/13 14:56:11 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/ft_printf.h"
+
+int	ft_printf_c(t_args *args)
+{
+	int		ret;
+	char	c;
+
+	ret = 0;
+	c = va_arg(args->ap, int);
+	ret += ft_outchar(c);
+	if (ret == -1)
+		args->error_flag++;
+	return (ret);
+}
---
+++ lib/srcs/ft_printf/srcs/ft_printf_u.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_u.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 03:04:57 by kinamura          #+#    #+#             */
+/*   Updated: 2024/08/13 14:56:44 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/ft_printf.h"
+
+int	ft_printf_u(t_args *args)
+{
+	int				ret;
+	unsigned int	num;
+
+	ret = 0;
+	num = va_arg(args->ap, unsigned int);
+	ret += ft_putunbr_base(num, BASE_10);
+	return (ret);
+}
