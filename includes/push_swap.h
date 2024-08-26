2014126da618155be5915f44b3dac126bdc9b7ab
kinamura <i.kosuke0801@gmail.com>
2024-08-27 02:00:26 +0900

    create push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 01:22:35 by kinamura          #+#    #+#             */
+/*   Updated: 2024/08/27 01:29:07 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "libft.h"
-#include "get_next_line.h"
-#include "ft_printf.h"
+# include "libft.h"
+# include "get_next_line.h"
+# include "ft_printf.h"
 
-int main(void);
+# include "ft_stack.h"
+# include "ft_utils.h"
+
+int	main(int argc, char **argv);
 
 #endif
