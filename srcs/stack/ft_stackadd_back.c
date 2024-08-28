e23c849f49c9cddd97be370860b68e8463c5c398
kinamura <i.kosuke0801@gmail.com>
2024-08-28 18:17:23 +0900

    create ft_stackadd_back.c

--- srcs/stack/ft_stackadd_back.c
+++ srcs/stack/ft_stackadd_back.c
@@ -6,24 +6,25 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 02:40:45 by kinamura          #+#    #+#             */
-/*   Updated: 2024/08/27 03:15:00 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/08/28 14:41:18 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "ft_stack.h"
 
 void	ft_stackadd_back(t_stack **stack, t_stack *new)
 {
+	t_stack	*last;
+
 	if (!new)
 		ft_stackfree(stack);
 	if (*stack)
 	{
-		new->next = *stack;
-		(*stack)->prev = new;
-		*stack = new;
+		last = ft_stacklast(stack);
+		new->next = last->next;
+		last->next = new;
+		new->prev = last;
 	}
 	else
-	{
 		*stack = new;
-	}
 }
