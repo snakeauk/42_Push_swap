9c688de258711d64dd360edc0d03f51872d6718d
kinamura <kinamura@student.42tokyo.jp>
2024-10-14 13:58:16 +0900

    added Error

--- srcs/ft_array/ft_argv_input_array.c
+++ srcs/ft_array/ft_argv_input_array.c
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 00:46:06 by kinamura          #+#    #+#             */
-/*   Updated: 2024/10/10 00:51:37 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/10/14 13:57:02 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,22 @@ int	*ft_argv_input_array(int argc, char **argv)
 	{
 		split = ft_split(argv[1], ' ');
 		if (!split)
+		{
+			ft_printf("Error\n");
 			return (NULL);
+		}
 		array = ft_array_input(0, ft_array_char_size(split), split);
 	}
 	else
 		array = ft_array_input(1, argc, argv);
+	if (!array)
+	{
+		ft_printf("Error\n");
+		return (NULL);
+	}
 	if (!ft_input_check(&array))
 	{
+		ft_printf("Error\n");
 		free(array);
 		return (NULL);
 	}
